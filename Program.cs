451b63bc318df5f6239093cde160c901549e471f
Malec7 <tomasdias.pt@gmail.com>
2025-10-22 02:05:27 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,446 +1,153 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-
-// EXERCISE 1 - ARRAYS
-
-
-int[] numbers = new int[10];
-for (int i = 0; i < numbers.Length; i++)
+// 1 
+void ShowNumbers(int n)
 {
-    numbers[i] = rand.Next(10, 91);
-}
-
-// a) 
-bool found = false;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] == 50)
+    if (n == 0)
     {
-        Console.WriteLine("Found 50!");
-        found = true;
-        break;
+        Console.WriteLine(0);
+        return;
     }
-}
-if (!found)
-{
-    Console.WriteLine("50 not found.");
-}
 
-// b)
-int count50 = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] == 50)
+    if (n > 0)
     {
-        count50++;
-    }
-}
-Console.WriteLine("50 appears " + count50 + " times.");
-
-// c) 
-float average = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    average += numbers[i];
-}
-average = average / numbers.Length;
-Console.WriteLine("Average: " + average);
-
-// d) 
-int min = numbers[0];
-int max = numbers[0];
-for (int i = 1; i < numbers.Length; i++)
-{
-    if (numbers[i] < min)
-        min = numbers[i];
-    if (numbers[i] > max)
-        max = numbers[i];
-}
-Console.WriteLine("Min: " + min + ", Max: " + max);
-
-// e)
-long sum = 0;
-long product = 1;
-for (int i = 0; i < numbers.Length; i++)
-{
-    sum += numbers[i];
-    product *= numbers[i];
-}
-Console.WriteLine("Sum: " + sum + ", Product: " + product);
-
-// f) 
-Console.Write("Reverse order: ");
-for (int i = numbers.Length - 1; i >= 0; i--)
-{
-    Console.Write(numbers[i] + " ");
-}
-Console.WriteLine();
-
-// g) 
-int[] reversedNumbers = new int[numbers.Length];
-for (int i = 0; i < numbers.Length; i++)
-{
-    reversedNumbers[i] = numbers[numbers.Length - 1 - i];
-}
-
-// h) 
-int evenCount = 0, oddCount = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] % 2 == 0)
-        evenCount++;
-    else
-        oddCount++;
-}
-
-int[] evenNumbers = new int[evenCount];
-int[] oddNumbers = new int[oddCount];
-
-int e = 0, o = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] % 2 == 0)
-        evenNumbers[e++] = numbers[i];
-    else
-        oddNumbers[o++] = numbers[i];
-}
-
-
-// EXERCISE 2 
-
-
-Console.Write("\nEnter how many integers (n > 0): ");
-int n = int.Parse(Console.ReadLine());
-int[] values = new int[n];
-for (int i = 0; i < n; i++)
-{
-    Console.Write("Value " + (i + 1) + ": ");
-    values[i] = int.Parse(Console.ReadLine());
-}
-Console.Write("Result: ");
-for (int i = 0; i < n; i++)
-{
-    Console.Write(values[i] * i + " ");
-}
-Console.WriteLine();
-
-
-// EXERCISE 3 
-
-
-Console.Write("\nEnter number of throws: ");
-int throwsN = int.Parse(Console.ReadLine());
-int[] throwsCount = new int[6];
-for (int i = 0; i < throwsN; i++)
-{
-    int face = rand.Next(1, 7);
-    throwsCount[face - 1]++;
-}
-
-Console.WriteLine("Number of throws: " + throwsN);
-for (int i = 0; i < 6; i++)
-{
-    double percent = (double)throwsCount[i] / throwsN * 100;
-    Console.WriteLine("Face " + (i + 1) + ": " + throwsCount[i] + " (" + percent.ToString("F2") + "%)");
-}
-
-
-// EXERCISE 4 
-
-
-int[,] matrix = new int[4, 4];
-Console.WriteLine("\nEnter 16 integers (4x4 matrix):");
-for (int i = 0; i < 4; i++)
-{
-    for (int j = 0; j < 4; j++)
-    {
-        matrix[i, j] = int.Parse(Console.ReadLine());
+        ShowNumbers(n - 1);
+        Console.WriteLine(n);
     }
 }
 
-int totalSum = 0;
-for (int i = 0; i < 4; i++)
-    for (int j = 0; j < 4; j++)
-        totalSum += matrix[i, j];
-Console.WriteLine("Total sum: " + totalSum);
+ShowNumbers(5); // prints 0 1 2 3 4 5
 
-int sumRow2 = 0;
-for (int j = 0; j < 4; j++)
-    sumRow2 += matrix[1, j];
-Console.WriteLine("Sum of 2nd row: " + sumRow2);
-
-int sumCol3 = 0;
-for (int i = 0; i < 4; i++)
-    sumCol3 += matrix[i, 2];
-Console.WriteLine("Sum of 3rd column: " + sumCol3);
-
-Console.Write("Main diagonal: ");
-for (int i = 0; i < 4; i++)
-    Console.Write(matrix[i, i] + " ");
-Console.WriteLine();
-
-Console.Write("Secondary diagonal: ");
-for (int i = 0; i < 4; i++)
-    Console.Write(matrix[i, 3 - i] + " ");
-Console.WriteLine();
-
-
-// EXERCISE 5 
-
-
-Console.Write("\nEnter size n for square matrix: ");
-int size = int.Parse(Console.ReadLine());
-int[,] square = new int[size, size];
-
-Console.WriteLine("Enter " + (size * size) + " integers:");
-for (int i = 0; i < size; i++)
-    for (int j = 0; j < size; j++)
-        square[i, j] = int.Parse(Console.ReadLine());
-
-int refSum = 0;
-for (int j = 0; j < size; j++)
-    refSum += square[0, j];
-
-bool isMagic = true;
-int sumDiag1 = 0, sumDiag2 = 0;
-
-// Check rows
-for (int i = 0; i < size; i++)
+// 2 
+int SumArray(int[] nums, int pos)
 {
-    int sumRow = 0;
-    for (int j = 0; j < size; j++)
-        sumRow += square[i, j];
-    if (sumRow != refSum)
-        isMagic = false;
+    if (pos == nums.Length)
+        return 0;
+
+    return nums[pos] + SumArray(nums, pos + 1);
 }
 
-// Check columns
-for (int j = 0; j < size; j++)
+int[] values = { 1, 2, 3, 4 };
+Console.WriteLine("Array sum: " + SumArray(values, 0));
+
+// 3 
+int AddUpTo(int n)
 {
-    int sumCol = 0;
-    for (int i = 0; i < size; i++)
-        sumCol += square[i, j];
-    if (sumCol != refSum)
-        isMagic = false;
+    if (n <= 0)
+        return 0;
+
+    return n + AddUpTo(n - 1);
 }
 
-// Check diagonals
-for (int i = 0; i < size; i++)
+Console.WriteLine("Sum to N: " + AddUpTo(5));
+
+// 4 
+string ReverseTxt(string txt, int pos)
 {
-    sumDiag1 += square[i, i];
-    sumDiag2 += square[i, size - 1 - i];
+    if (pos == txt.Length)
+        return "";
+
+    string rest = ReverseTxt(txt, pos + 1);
+    return rest + txt[pos];
 }
 
-if (sumDiag1 != refSum || sumDiag2 != refSum)
-    isMagic = false;
+Console.WriteLine("Reversed: " + ReverseTxt("hello", 0));
 
-if (isMagic)
-    Console.WriteLine("Magic square!");
-else
-    Console.WriteLine("Not a magic square.");
-
-
-// LIST 
-
-
-List<int> list = new List<int>();
-int option = 0;
-
-do
+// 5 
+long Fact(int n)
 {
-    Console.WriteLine("\n=== LIST ===");
-    Console.WriteLine("1) Add number");
-    Console.WriteLine("2) Remove number");
-    Console.WriteLine("3) Remove by position");
-    Console.WriteLine("4) Print list");
-    Console.WriteLine("5) Print list in reverse order");
-    Console.WriteLine("6) Count elements");
-    Console.WriteLine("7) Clear list");
-    Console.WriteLine("8) Exit");
-    Console.Write("Option: ");
+    if (n <= 1)
+        return 1;
 
-    if (!int.TryParse(Console.ReadLine(), out option))
-    {
-        Console.WriteLine("Invalid input.");
-        continue;
-    }
+    return n * Fact(n - 1);
+}
 
-    if (option == 1)
+Console.WriteLine("Factorial: " + Fact(5));
+
+// 6 
+long SuperFact(int n)
+{
+    long total = 1;
+
+    for (int i = 1; i <= n; i++)
     {
-        Console.Write("Enter number: ");
-        int value = int.Parse(Console.ReadLine());
-        list.Add(value);
-    }
-    else if (option == 2)
-    {
-        Console.Write("Enter number to remove: ");
-        int value = int.Parse(Console.ReadLine());
-        if (list.Contains(value))
+        long temp = 1;
+        for (int j = 1; j <= i; j++)
         {
-            list.Remove(value);
-            Console.WriteLine("Number removed.");
+            temp *= j;
         }
-        else
-            Console.WriteLine("Number not found.");
-    }
-    else if (option == 3)
-    {
-        Console.Write("Enter position: ");
-        int pos = int.Parse(Console.ReadLine());
-        if (pos >= 0 && pos < list.Count)
-        {
-            list.RemoveAt(pos);
-            Console.WriteLine("Element removed.");
-        }
-        else
-            Console.WriteLine("Invalid position.");
-    }
-    else if (option == 4)
-    {
-        Console.Write("List: ");
-        for (int i = 0; i < list.Count; i++)
-            Console.Write(list[i] + " ");
-        Console.WriteLine();
-    }
-    else if (option == 5)
-    {
-        Console.Write("Reversed: ");
-        for (int i = list.Count - 1; i >= 0; i--)
-            Console.Write(list[i] + " ");
-        Console.WriteLine();
-    }
-    else if (option == 6)
-    {
-        Console.WriteLine("List has " + list.Count + " elements.");
-    }
-    else if (option == 7)
-    {
-        list.Clear();
-        Console.WriteLine("List cleared.");
+        total *= temp;
     }
 
-} while (option != 8);
+    return total;
+}
 
+Console.WriteLine("Super factorial: " + SuperFact(4));
 
-// STACK MENU
-
-
-Stack<int> stack = new Stack<int>();
-option = 0;
-
-do
+// 7 
+int TetraSeq(int n)
 {
-    Console.WriteLine("\n=== STACK MENU ===");
-    Console.WriteLine("1) Push a number");
-    Console.WriteLine("2) Pop a number");
-    Console.WriteLine("3) Peek (show top)");
-    Console.WriteLine("4) Count elements");
-    Console.WriteLine("5) Clear stack");
-    Console.WriteLine("6) Exit");
-    Console.Write("Option: ");
+    if (n == 0 || n == 1 || n == 2)
+        return 0;
+    if (n == 3)
+        return 1;
 
-    if (!int.TryParse(Console.ReadLine(), out option))
+    int[] t = new int[n + 1];
+    t[0] = 0; t[1] = 0; t[2] = 0; t[3] = 1;
+
+    for (int i = 4; i <= n; i++)
     {
-        Console.WriteLine("Invalid input.");
-        continue;
+        t[i] = t[i - 1] + t[i - 2] + t[i - 3] + t[i - 4];
     }
 
-    if (option == 1)
-    {
-        Console.Write("Enter number: ");
-        int value = int.Parse(Console.ReadLine());
-        stack.Push(value);
-        Console.WriteLine("Number pushed.");
-    }
-    else if (option == 2)
-    {
-        if (stack.Count > 0)
-            Console.WriteLine("Popped: " + stack.Pop());
-        else
-            Console.WriteLine("Stack is empty.");
-    }
-    else if (option == 3)
-    {
-        if (stack.Count > 0)
-            Console.WriteLine("Top: " + stack.Peek());
-        else
-            Console.WriteLine("Stack is empty.");
-    }
-    else if (option == 4)
-    {
-        Console.WriteLine("Stack has " + stack.Count + " elements.");
-    }
-    else if (option == 5)
-    {
-        stack.Clear();
-        Console.WriteLine("Stack cleared.");
-    }
+    return t[n];
+}
 
-} while (option != 6);
+Console.WriteLine("Tetranacci: " + TetraSeq(4));
 
-
-// QUEUE 
-
-
-Queue<string> queue = new Queue<string>();
-option = 0;
-
-do
+// 8 
+long CubesSum(int n)
 {
-    Console.WriteLine("\n=== QUEUE MENU ===");
-    Console.WriteLine("1) Enqueue string");
-    Console.WriteLine("2) Dequeue string");
-    Console.WriteLine("3) Peek (show front)");
-    Console.WriteLine("4) Print all elements");
-    Console.WriteLine("5) Count elements");
-    Console.WriteLine("6) Clear queue");
-    Console.WriteLine("7) Exit");
-    Console.Write("Option: ");
+    if (n <= 0)
+        return 0;
 
-    if (!int.TryParse(Console.ReadLine(), out option))
-    {
-        Console.WriteLine("Invalid input.");
-        continue;
-    }
+    return (long)(n * n * n) + CubesSum(n - 1);
+}
 
-    if (option == 1)
+Console.WriteLine("Sum of cubes: " + CubesSum(3));
+
+// 9 
+int DigitsSum(int num)
+{
+    if (num == 0)
+        return 0;
+
+    int last = num % 10;
+    int rest = num / 10;
+
+    return last + DigitsSum(rest);
+}
+
+Console.WriteLine("Sum of digits: " + DigitsSum(123));
+
+// 10 
+int PellNum(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    int[] p = new int[n + 1];
+    p[0] = 0;
+    p[1] = 1;
+
+    for (int i = 2; i <= n; i++)
     {
-        Console.Write("Enter string: ");
-        string text = Console.ReadLine();
-        queue.Enqueue(text);
-        Console.WriteLine("String enqueued.");
-    }
-    else if (option == 2)
-    {
-        if (queue.Count > 0)
-            Console.WriteLine("Dequeued: " + queue.Dequeue());
-        else
-            Console.WriteLine("Queue is empty.");
-    }
-    else if (option == 3)
-    {
-        if (queue.Count > 0)
-            Console.WriteLine("Front: " + queue.Peek());
-        else
-            Console.WriteLine("Queue is empty.");
-    }
-    else if (option == 4)
-    {
-        Console.Write("Queue contents: ");
-        foreach (string s in queue)
-            Console.Write(s + " ");
-        Console.WriteLine();
-    }
-    else if (option == 5)
-    {
-        Console.WriteLine("Queue has " + queue.Count + " elements.");
-    }
-    else if (option == 6)
-    {
-        queue.Clear();
-        Console.WriteLine("Queue cleared.");
+        p[i] = 2 * p[i - 1] + p[i - 2];
     }
 
-} while (option != 7);
-    
+    return p[n];
+}
+
+Console.WriteLine("Pell number: " + PellNum(7));
